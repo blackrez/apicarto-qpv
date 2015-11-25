793aa39c490e9a82fdd382b8024980a5d0a06e26
Nabil Servais <nabil@eml.cc>
2015-11-25 19:00:21 +0100

    Remove useless code in helpers for geojson

--- helpers/geojson.js
+++ helpers/geojson.js
@@ -6,17 +6,18 @@ function FeatureCollection() {
 
 exports.rows_to_geojson = function(rows, propertiesObject) {
   var featureCollection = new FeatureCollection();
-  rows.map(function (row){
+  featureCollection.features = rows.map(function (row){
     var properties = {};
     for (var p in propertiesObject) {
       namePropertie = propertiesObject[p];
       properties[namePropertie] = row[namePropertie];
     }
-    featureCollection.features.push({
+    return {
       type: "Feature",
       geometry: JSON.parse(row.geom),
       properties: properties
-    })
+    }
   })
+
   return featureCollection
 }
